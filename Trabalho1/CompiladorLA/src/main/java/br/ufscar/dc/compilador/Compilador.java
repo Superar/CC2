e135fb7a899d45f6ee093abb63adc3fa7e054566
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-08-13 13:11:38 -0300

    Recebimento de arquivo como parâmetro do compilador

--- Trabalho1/CompiladorLA/src/main/java/br/ufscar/dc/compilador/Compilador.java
+++ Trabalho1/CompiladorLA/src/main/java/br/ufscar/dc/compilador/Compilador.java
@@ -2,16 +2,28 @@ package br.ufscar.dc.compilador;
 
 import org.antlr.v4.runtime.*;
 import br.ufscar.dc.antlr.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Compilador {
 
     public static void main(String[] args) {
-        String teste = "a = b + c";
-        ANTLRInputStream input = new ANTLRInputStream(teste);
-        LALexer lexer = new LALexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        LAParser parser = new LAParser(tokens);
-        LAParser.ProgramaContext arvore = parser.programa();
-        System.out.println(arvore.start);
+        FileInputStream caminho;
+
+        try {
+            caminho = new FileInputStream(args[0]);
+            ANTLRInputStream input = new ANTLRInputStream(caminho);
+            LALexer lexer = new LALexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            LAParser parser = new LAParser(tokens);
+            LAParser.ProgramaContext arvore = parser.programa();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Compilador.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Compilador.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }

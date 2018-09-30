d15d89cdd79d4abf296e3f9bcc8429f5b3ba9185
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-09-30 17:56:43 -0300

    Inclusão de exemplo da linguagem e saída

---
+++ Trabalho2/Exemplos/exemplo.cr
@@ -0,0 +1,27 @@
+Cronograma FrufflesExemplo {
+    Atividades {
+        Atividade Atividade1 {
+            "Fruffles"
+        },
+        Atividade Atividade2 {
+            "Fruffles 2"
+        },
+        Atividade Atividade3 {
+            "Fruffles 3"
+        }
+    },
+    Datas {
+        Atividade Atividade1 {
+            Inicio: 23/09/2018,
+            Fim: 30/09/2018
+        },
+        Atividade Atividade2 {
+            Inicio: 23/09/2018,
+            Fim: 30/09/2018
+        },
+        Atividade Atividade3 {
+            Inicio: 23/09/2018,
+            Fim: 23/09/2018
+        }
+    }
+}

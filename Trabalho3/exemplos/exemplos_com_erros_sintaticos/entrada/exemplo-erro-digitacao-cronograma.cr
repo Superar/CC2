545133ae317bf7b588443c713e9129f199efd534
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-11-07 21:23:28 -0200

    Inclusão de exemplos de erros sintáticos

---
+++ Trabalho3/exemplos/exemplos_com_erros_sintaticos/entrada/exemplo-erro-digitacao-cronograma.cr
@@ -0,0 +1,8 @@
+Cornograma {
+    Atividade Atividade1 {
+        "Descricao da atividade 1",
+        Datas {
+            07/11/2018 - 07/11/2018
+        }
+    }
+}
---
+++ Trabalho3/exemplos/exemplos_com_erros_sintaticos/entrada/exemplo-erro-identificador.cr
@@ -0,0 +1,22 @@
+Cronograma 0ExemploComErro {
+    Atividades {
+        Atividade Atividade1 {
+            "Texto da atividade 1",
+            Datas {
+                23/09/2018 - 30/09/2018
+            }
+        },
+        Atividade Atividade2 {
+            "Texto da atividade 2",
+            Datas {
+                23/09/2018 - 30/09/2018
+            }
+        },
+        Atividade Atividade3 {
+            "Texto da atividade 3",
+            Datas {
+                23/09/2018 - 23/09/2018
+            }
+        }
+    }
+}

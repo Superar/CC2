bff7c441d3683784af7136a9e0b12820ed6d1f81
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-11-07 21:41:01 -0200

    Inclusão de exemplos iniciais com erro semântico

---
+++ Trabalho3/exemplos/exemplos_com_erros_semanticos/entrada/exemplo-erro-atividade-duplicada.cr
@@ -0,0 +1,17 @@
+Cronograma ExemploDuplicado {
+    Atividades {
+        Atividade atv1 {
+            "Descricao",
+            Datas {
+                07/11/2018 - 07/11/2018
+            }
+        },
+        Atividade atv1 {
+            "Atividade 1 com outra descricao e datas",
+            Datas {
+                25/12/2018 - 10/01/2019
+                15/01/2019 - 02/02/2019
+            }
+        }
+    }
+}
---
+++ Trabalho3/exemplos/exemplos_com_erros_semanticos/entrada/exemplo-erro-cronograma-duplicado.cr
@@ -0,0 +1,21 @@
+Cronograma ExemploDuplicado {
+    Atividades {
+        Atividade atv1 {
+            "Descricao",
+            Datas {
+                07/11/2018 - 07/11/2018
+            }
+        }
+    }
+}
+
+Cronograma ExemploDuplicado {
+    Atividades {
+        Atividade outraAtividade {
+            "Descricao a outra atividade",
+            Datas {
+                25/12/2018 - 10/01/2019
+            }
+        }
+    }
+}
